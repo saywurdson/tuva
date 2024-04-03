e35a2005fccccbbd3ad1ac19ae7811f4925e5612
saywurdson <34385409+saywurdson@users.noreply.github.com>
2024-04-02 20:25:53 -0400

    add ffs to eligibility

--- models/tuva_input/eligibility.sql
+++ models/tuva_input/eligibility.sql
@@ -1,11 +1,15 @@
 -- models/eligibility.sql
 
 SELECT DISTINCT
-    CAST(MD5(inst.employee_date_of_birth || inst.employee_gender_code) AS VARCHAR) AS patient_id,
+    CAST(MD5(instc.employee_date_of_birth || instc.employee_gender_code) AS VARCHAR) AS patient_id,
     CAST(NULL AS VARCHAR) AS member_id,
-    CAST(inst.employee_gender_code AS VARCHAR) AS gender,
+    CASE
+        WHEN instc.employee_gender_code = 'M' THEN 'male'
+        WHEN instc.employee_gender_code = 'F' THEN 'female'
+        ELSE 'unknown'
+    END AS gender,
     CAST(NULL AS VARCHAR) AS race,
-    CAST(inst.employee_date_of_birth AS DATE) AS birth_date,
+    CAST(instc.employee_date_of_birth AS DATE) AS birth_date,
     CAST(NULL AS DATE) AS death_date,
     CAST(FALSE AS BOOLEAN) AS death_flag,
     CAST(NULL AS DATE) AS enrollment_start_date,
@@ -19,21 +23,25 @@ SELECT DISTINCT
     CAST(NULL AS VARCHAR) AS first_name,
     CAST(NULL AS VARCHAR) AS last_name,
     CAST(NULL AS VARCHAR) AS address,
-    CAST(inst.employee_mailing_city AS VARCHAR) AS city,
-    CAST(inst.employee_mailing_state_code AS VARCHAR) AS state,
-    CAST(inst.billing_provider_postal_code AS VARCHAR) AS zip_code,
+    CAST(instc.employee_mailing_city AS VARCHAR) AS city,
+    CAST(instc.employee_mailing_state_code AS VARCHAR) AS state,
+    CAST(instc.billing_provider_postal_code AS VARCHAR) AS zip_code,
     CAST(NULL AS VARCHAR) AS phone,
     'Texas Department of Insurance, Division of Workers Compensation (DWC)' AS data_source
-FROM {{ source('tx', 'institutional_header') }} inst
+FROM {{ source('tx', 'institutional_header_current') }} instc
 
 UNION
 
 SELECT DISTINCT
-    CAST(MD5(prof.employee_date_of_birth || prof.employee_gender_code) AS VARCHAR) AS patient_id,
+    CAST(MD5(insth.employee_date_of_birth || insth.employee_gender_code) AS VARCHAR) AS patient_id,
     CAST(NULL AS VARCHAR) AS member_id,
-    CAST(prof.employee_gender_code AS VARCHAR) AS gender,
+    CASE
+        WHEN insth.employee_gender_code = 'M' THEN 'male'
+        WHEN insth.employee_gender_code = 'F' THEN 'female'
+        ELSE 'unknown'
+    END AS gender,
     CAST(NULL AS VARCHAR) AS race,
-    CAST(prof.employee_date_of_birth AS DATE) AS birth_date,
+    CAST(insth.employee_date_of_birth AS DATE) AS birth_date,
     CAST(NULL AS DATE) AS death_date,
     CAST(FALSE AS BOOLEAN) AS death_flag,
     CAST(NULL AS DATE) AS enrollment_start_date,
@@ -47,21 +55,25 @@ SELECT DISTINCT
     CAST(NULL AS VARCHAR) AS first_name,
     CAST(NULL AS VARCHAR) AS last_name,
     CAST(NULL AS VARCHAR) AS address,
-    CAST(prof.employee_mailing_city AS VARCHAR) AS city,
-    CAST(prof.employee_mailing_state_code AS VARCHAR) AS state,
-    CAST(prof.billing_provider_postal_code AS VARCHAR) AS zip_code,
+    CAST(insth.employee_mailing_city AS VARCHAR) AS city,
+    CAST(insth.employee_mailing_state_code AS VARCHAR) AS state,
+    CAST(insth.billing_provider_postal_code AS VARCHAR) AS zip_code,
     CAST(NULL AS VARCHAR) AS phone,
     'Texas Department of Insurance, Division of Workers Compensation (DWC)' AS data_source
-FROM {{ source('tx', 'professional_header') }} prof
+FROM {{ source('tx', 'institutional_header_historical') }} insth
 
 UNION
 
 SELECT DISTINCT
-    CAST(MD5(pharm.employee_date_of_birth || pharm.employee_gender_code) AS VARCHAR) AS patient_id,
+    CAST(MD5(profc.employee_date_of_birth || profc.employee_gender_code) AS VARCHAR) AS patient_id,
     CAST(NULL AS VARCHAR) AS member_id,
-    CAST(pharm.employee_gender_code AS VARCHAR) AS gender,
+    CASE
+        WHEN profc.employee_gender_code = 'M' THEN 'male'
+        WHEN profc.employee_gender_code = 'F' THEN 'female'
+        ELSE 'unknown'
+    END AS gender,
     CAST(NULL AS VARCHAR) AS race,
-    CAST(pharm.employee_date_of_birth AS DATE) AS birth_date,
+    CAST(profc.employee_date_of_birth AS DATE) AS birth_date,
     CAST(NULL AS DATE) AS death_date,
     CAST(FALSE AS BOOLEAN) AS death_flag,
     CAST(NULL AS DATE) AS enrollment_start_date,
@@ -75,9 +87,224 @@ SELECT DISTINCT
     CAST(NULL AS VARCHAR) AS first_name,
     CAST(NULL AS VARCHAR) AS last_name,
     CAST(NULL AS VARCHAR) AS address,
-    CAST(pharm.employee_mailing_city AS VARCHAR) AS city,
-    CAST(pharm.employee_mailing_state_code AS VARCHAR) AS state,
-    CAST(pharm.billing_provider_postal_code AS VARCHAR) AS zip_code,
+    CAST(profc.employee_mailing_city AS VARCHAR) AS city,
+    CAST(profc.employee_mailing_state_code AS VARCHAR) AS state,
+    CAST(profc.billing_provider_postal_code AS VARCHAR) AS zip_code,
     CAST(NULL AS VARCHAR) AS phone,
     'Texas Department of Insurance, Division of Workers Compensation (DWC)' AS data_source
-FROM {{ source('tx', 'pharmacy_header') }} pharm
+FROM {{ source('tx', 'professional_header_current') }} profc
+
+UNION
+
+SELECT DISTINCT
+    CAST(MD5(profh.employee_date_of_birth || profh.employee_gender_code) AS VARCHAR) AS patient_id,
+    CAST(NULL AS VARCHAR) AS member_id,
+    CASE
+        WHEN profh.employee_gender_code = 'M' THEN 'male'
+        WHEN profh.employee_gender_code = 'F' THEN 'female'
+        ELSE 'unknown'
+    END AS gender,
+    CAST(NULL AS VARCHAR) AS race,
+    CAST(profh.employee_date_of_birth AS DATE) AS birth_date,
+    CAST(NULL AS DATE) AS death_date,
+    CAST(FALSE AS BOOLEAN) AS death_flag,
+    CAST(NULL AS DATE) AS enrollment_start_date,
+    CAST(NULL AS DATE) AS enrollment_end_date,
+    CAST(NULL AS VARCHAR) AS payer,
+    'self-insured' AS payer_type,
+    CAST(NULL AS VARCHAR) AS plan,
+    CAST(NULL AS VARCHAR) AS original_reason_entitlement_code,
+    'NA' AS dual_status_code,
+    CAST(NULL AS VARCHAR) AS medicare_status_code,
+    CAST(NULL AS VARCHAR) AS first_name,
+    CAST(NULL AS VARCHAR) AS last_name,
+    CAST(NULL AS VARCHAR) AS address,
+    CAST(profh.employee_mailing_city AS VARCHAR) AS city,
+    CAST(profh.employee_mailing_state_code AS VARCHAR) AS state,
+    CAST(profh.billing_provider_postal_code AS VARCHAR) AS zip_code,
+    CAST(NULL AS VARCHAR) AS phone,
+    'Texas Department of Insurance, Division of Workers Compensation (DWC)' AS data_source
+FROM {{ source('tx', 'professional_header_historical') }} profh
+
+UNION
+
+SELECT DISTINCT
+    CAST(MD5(phc.employee_date_of_birth || phc.employee_gender_code) AS VARCHAR) AS patient_id,
+    CAST(NULL AS VARCHAR) AS member_id,
+    CASE
+        WHEN phc.employee_gender_code = 'M' THEN 'male'
+        WHEN phc.employee_gender_code = 'F' THEN 'female'
+        ELSE 'unknown'
+    END AS gender,
+    CAST(NULL AS VARCHAR) AS race,
+    CAST(phc.employee_date_of_birth AS DATE) AS birth_date,
+    CAST(NULL AS DATE) AS death_date,
+    CAST(FALSE AS BOOLEAN) AS death_flag,
+    CAST(NULL AS DATE) AS enrollment_start_date,
+    CAST(NULL AS DATE) AS enrollment_end_date,
+    CAST(NULL AS VARCHAR) AS payer,
+    'self-insured' AS payer_type,
+    CAST(NULL AS VARCHAR) AS plan,
+    CAST(NULL AS VARCHAR) AS original_reason_entitlement_code,
+    'NA' AS dual_status_code,
+    CAST(NULL AS VARCHAR) AS medicare_status_code,
+    CAST(NULL AS VARCHAR) AS first_name,
+    CAST(NULL AS VARCHAR) AS last_name,
+    CAST(NULL AS VARCHAR) AS address,
+    CAST(phc.employee_mailing_city AS VARCHAR) AS city,
+    CAST(phc.employee_mailing_state_code AS VARCHAR) AS state,
+    CAST(phc.billing_provider_postal_code AS VARCHAR) AS zip_code,
+    CAST(NULL AS VARCHAR) AS phone,
+    'Texas Department of Insurance, Division of Workers Compensation (DWC)' AS data_source
+FROM {{ source('tx', 'pharmacy_header_current') }} phc
+
+UNION
+
+SELECT DISTINCT
+    CAST(MD5(phh.employee_date_of_birth || phh.employee_gender_code) AS VARCHAR) AS patient_id,
+    CAST(NULL AS VARCHAR) AS member_id,
+    CASE
+        WHEN phh.employee_gender_code = 'M' THEN 'male'
+        WHEN phh.employee_gender_code = 'F' THEN 'female'
+        ELSE 'unknown'
+    END AS gender,
+    CAST(NULL AS VARCHAR) AS race,
+    CAST(phh.employee_date_of_birth AS DATE) AS birth_date,
+    CAST(NULL AS DATE) AS death_date,
+    CAST(FALSE AS BOOLEAN) AS death_flag,
+    CAST(NULL AS DATE) AS enrollment_start_date,
+    CAST(NULL AS DATE) AS enrollment_end_date,
+    CAST(NULL AS VARCHAR) AS payer,
+    'self-insured' AS payer_type,
+    CAST(NULL AS VARCHAR) AS plan,
+    CAST(NULL AS VARCHAR) AS original_reason_entitlement_code,
+    'NA' AS dual_status_code,
+    CAST(NULL AS VARCHAR) AS medicare_status_code,
+    CAST(NULL AS VARCHAR) AS first_name,
+    CAST(NULL AS VARCHAR) AS last_name,
+    CAST(NULL AS VARCHAR) AS address,
+    CAST(phh.employee_mailing_city AS VARCHAR) AS city,
+    CAST(phh.employee_mailing_state_code AS VARCHAR) AS state,
+    CAST(phh.billing_provider_postal_code AS VARCHAR) AS zip_code,
+    CAST(NULL AS VARCHAR) AS phone,
+    'Texas Department of Insurance, Division of Workers Compensation (DWC)' AS data_source
+FROM {{ source('tx', 'pharmacy_header_historical') }} phh
+
+UNION
+
+SELECT DISTINCT
+    CAST(ffs.BENE_ID AS VARCHAR) AS patient_id,
+    CAST(NULL AS VARCHAR) AS member_id,
+    CASE
+        WHEN ffs.SEX_IDENT_CD = '1' THEN 'male'
+        WHEN ffs.SEX_IDENT_CD = '2' THEN 'female'
+        ELSE 'unknown'
+    END AS gender,
+    CASE
+        WHEN ffs.BENE_RACE_CD = '1' THEN 'white'
+        WHEN ffs.BENE_RACE_CD = '2' THEN 'black or african american'
+        WHEN ffs.BENE_RACE_CD = '3' THEN 'other race'
+        WHEN ffs.BENE_RACE_CD = '4' THEN 'asian'
+        WHEN ffs.BENE_RACE_CD = '5' THEN 'hispanic'
+        WHEN ffs.BENE_RACE_CD = '6' THEN 'american indian or alaska native'
+        ELSE 'unknown'
+    END AS race,
+    CAST(
+        SUBSTRING(ffs.BENE_BIRTH_DT, 8, 4) || '-' ||
+        CASE 
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'Jan' THEN '01'
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'Feb' THEN '02'
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'Mar' THEN '03'
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'Apr' THEN '04'
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'May' THEN '05'
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'Jun' THEN '06'
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'Jul' THEN '07'
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'Aug' THEN '08'
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'Sep' THEN '09'
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'Oct' THEN '10'
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'Nov' THEN '11'
+            WHEN SUBSTRING(ffs.BENE_BIRTH_DT, 4, 3) = 'Dec' THEN '12'
+            ELSE '00'
+        END || '-' || 
+        SUBSTRING(ffs.BENE_BIRTH_DT, 1, 2)
+        AS DATE
+    ) AS birth_date,
+    CAST(
+        SUBSTRING(ffs.BENE_DEATH_DT, 8, 4) || '-' ||
+        CASE
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'Jan' THEN '01'
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'Feb' THEN '02'
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'Mar' THEN '03'
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'Apr' THEN '04'
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'May' THEN '05'
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'Jun' THEN '06'
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'Jul' THEN '07'
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'Aug' THEN '08'
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'Sep' THEN '09'
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'Oct' THEN '10'
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'Nov' THEN '11'
+            WHEN SUBSTRING(ffs.BENE_DEATH_DT, 4, 3) = 'Dec' THEN '12'
+            ELSE '00'
+        END || '-' || 
+        SUBSTRING(ffs.BENE_DEATH_DT, 1, 2)
+        AS DATE
+    ) AS death_date,
+    CASE 
+        WHEN ffs.BENE_DEATH_DT IS NOT NULL THEN TRUE
+        ELSE FALSE
+    END AS death_flag,
+    CAST(
+        SUBSTRING(ffs.COVSTART, 8, 4) || '-' ||
+        CASE
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Jan' THEN '01'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Feb' THEN '02'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Mar' THEN '03'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Apr' THEN '04'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'May' THEN '05'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Jun' THEN '06'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Jul' THEN '07'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Aug' THEN '08'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Sep' THEN '09'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Oct' THEN '10'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Nov' THEN '11'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Dec' THEN '12'
+            ELSE '00'
+        END || '-' || 
+        SUBSTRING(ffs.COVSTART, 1, 2)
+        AS DATE
+    ) AS enrollment_start_date,
+    CAST(
+        SUBSTRING(ffs.COVSTART, 8, 4) || '-' || 
+        CASE 
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Jan' THEN '01'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Feb' THEN '02'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Mar' THEN '03'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Apr' THEN '04'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'May' THEN '05'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Jun' THEN '06'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Jul' THEN '07'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Aug' THEN '08'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Sep' THEN '09'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Oct' THEN '10'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Nov' THEN '11'
+            WHEN SUBSTRING(ffs.COVSTART, 4, 3) = 'Dec' THEN '12'
+            ELSE '00'
+        END || '-' || 
+        SUBSTRING(ffs.COVSTART, 1, 2)
+    AS DATE) + 
+    (ffs.BENE_HI_CVRAGE_TOT_MONS * INTERVAL '1' MONTH) AS enrollment_end_date,
+    CAST(NULL AS VARCHAR) AS payer,
+    'medicare' AS payer_type,
+    CAST(NULL AS VARCHAR) AS plan,
+    CAST(ffs.ENTLMT_RSN_ORIG AS VARCHAR) AS original_reason_entitlement_code,
+    CAST(ffs.DUAL_STUS_CD_12 AS VARCHAR) AS dual_status_code,
+    CAST(ffs.MDCR_STATUS_CODE_12 AS VARCHAR) AS medicare_status_code,
+    CAST(NULL AS VARCHAR) AS first_name,
+    CAST(NULL AS VARCHAR) AS last_name,
+    CAST(NULL AS VARCHAR) AS address,
+    CAST(NULL AS VARCHAR) AS city,
+    CAST(ffs.STATE_CODE AS VARCHAR) AS state,
+    CAST(ffs.ZIP_CD AS VARCHAR) AS zip_code,
+    CAST(NULL AS VARCHAR) AS phone,
+    'CMS Synthetic Medicare Enrollment, Fee-for-Service Claims, and Prescription Drug Event Data' AS data_source
+FROM {{ source('ffs', 'beneficiary') }} ffs
--- models/tuva_input/pharmacy_claim.sql
+++ models/tuva_input/pharmacy_claim.sql
@@ -1,25 +1,51 @@
 -- models/pharmacy_claim.sql
 
 SELECT DISTINCT
-    CAST(header.bill_id AS VARCHAR) AS claim_id,
-    CAST(detail.line_number AS INTEGER) AS claim_line_number,
-    CAST(MD5(header.employee_date_of_birth || header.employee_gender_code) AS VARCHAR) AS patient_id,
+    CAST(headerc.bill_id AS VARCHAR) AS claim_id,
+    CAST(detailc.line_number AS INTEGER) AS claim_line_number,
+    CAST(MD5(headerc.employee_date_of_birth || headerc.employee_gender_code) AS VARCHAR) AS patient_id,
     CAST(NULL AS VARCHAR) AS member_id,
     CAST(NULL AS VARCHAR) AS payer,
     CAST(NULL AS VARCHAR) AS plan,
-    CAST(header.billing_provider_national AS VARCHAR) AS prescribing_provider_npi,
-    CAST(COALESCE(detail.rendering_line_provider ,header.rendering_bill_provider_4) AS VARCHAR) AS dispensing_provider_npi,
-    CAST(detail.prescription_line_date AS DATE) AS dispensing_date,
-    CAST(detail.ndc_billed_code AS VARCHAR) AS ndc_code,
-    CAST(detail.drugs_supplies_quantity AS INTEGER) AS quantity,
-    CAST(detail.drugs_supplies_number_of AS INTEGER) AS days_supply,
+    CAST(headerc.billing_provider_national AS VARCHAR) AS prescribing_provider_npi,
+    CAST(COALESCE(detailc.rendering_line_provider, headerc.rendering_bill_provider_4) AS VARCHAR) AS dispensing_provider_npi,
+    CAST(detailc.prescription_line_date AS DATE) AS dispensing_date,
+    CAST(detailc.ndc_billed_code AS VARCHAR) AS ndc_code,
+    CAST(detailc.drugs_supplies_quantity AS INTEGER) AS quantity,
+    CAST(detailc.drugs_supplies_number_of AS INTEGER) AS days_supply,
     CAST(NULL AS INTEGER) AS refills,
-    CAST(header.date_insurer_paid_bill AS DATE) AS paid_date,
-    CAST(detail.total_amount_paid_per_line AS FLOAT) AS paid_amount,
+    CAST(headerc.date_insurer_paid_bill AS DATE) AS paid_date,
+    CAST(detailc.total_amount_paid_per_line AS FLOAT) AS paid_amount,
     CAST(NULL AS FLOAT) AS allowed_amount,
     CAST(NULL AS FLOAT) AS coinsurance_amount,
-    CAST(header.total_amount_paid_per_bill AS FLOAT) AS copayment_amount,
+    CAST(headerc.total_amount_paid_per_bill AS FLOAT) AS copayment_amount,
     CAST(NULL AS FLOAT) AS deductible_amount,
     'Texas Department of Insurance, Division of Workers Compensation (DWC)' AS data_source
-FROM {{ source('tx', 'pharmacy_header') }} header
-LEFT JOIN {{ source('tx', 'pharmacy_detail') }} detail ON header.bill_id = detail.bill_id
+FROM {{ source('tx', 'pharmacy_header_current') }} headerc
+LEFT JOIN {{ source('tx', 'pharmacy_detail_current') }} detailc ON headerc.bill_id = detailc.bill_id
+
+UNION
+
+SELECT DISTINCT
+    CAST(headerh.bill_id AS VARCHAR) AS claim_id,
+    CAST(detailh.line_number AS INTEGER) AS claim_line_number,
+    CAST(MD5(headerh.employee_date_of_birth || headerh.employee_gender_code) AS VARCHAR) AS patient_id,
+    CAST(NULL AS VARCHAR) AS member_id,
+    CAST(NULL AS VARCHAR) AS payer,
+    CAST(NULL AS VARCHAR) AS plan,
+    CAST(NULL AS VARCHAR) AS prescribing_provider_npi,
+    CAST(headerh.rendering_bill_provider_4 AS VARCHAR) AS dispensing_provider_npi,
+    CAST(detailh.prescription_line_date AS DATE) AS dispensing_date,
+    CAST(detailh.ndc_billed_code AS VARCHAR) AS ndc_code,
+    CAST(detailh.drugs_supplies_quantity AS INTEGER) AS quantity,
+    CAST(detailh.drugs_supplies_number_of AS INTEGER) AS days_supply,
+    CAST(NULL AS INTEGER) AS refills,
+    CAST(headerh.date_insurer_paid_bill AS DATE) AS paid_date,
+    CAST(detailh.total_amount_paid_per_line AS FLOAT) AS paid_amount,
+    CAST(NULL AS FLOAT) AS allowed_amount,
+    CAST(NULL AS FLOAT) AS coinsurance_amount,
+    CAST(headerh.total_amount_paid_per_bill AS FLOAT) AS copayment_amount,
+    CAST(NULL AS FLOAT) AS deductible_amount,
+    'Texas Department of Insurance, Division of Workers Compensation (DWC)' AS data_source
+FROM {{ source('tx', 'pharmacy_header_historical') }} headerh
+LEFT JOIN {{ source('tx', 'pharmacy_detail_historical') }} detailh ON headerh.bill_id = detailh.bill_id
